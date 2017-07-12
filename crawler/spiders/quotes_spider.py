0b1a62e999e86e59dca2f03d716e97c6507a7bca
Yuumao <xc648593557@gmail.com>
2017-07-12 19:31:31 +0800

    [update] query title and link

--- crawler/spiders/quotes_spider.py
+++ crawler/spiders/quotes_spider.py
@@ -3,18 +3,24 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-
+    allow_domains = ['blog.reimu.net']
+    start_urls = [
+        'https://blog.reimu.net',
+    ]
     def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
+        for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split('/')[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        # page = response.url.split('/')[-2]
+        # filename = 'quotes-%s.html' % page
+        # with open(filename, 'wb') as f:
+            # f.write(response.body)
+        # self.log('Saved file %s' % filename)
+        for article in response.css('article.hentry'):
+            img = article.css('div.entry-content').css('img')
+            yield {
+                'title': article.css('h2.entry-title').css('a::text').extract_first(),
+                'link':  article.css('h2.entry-title').css('a').re(r'href="(.*?)"')[0],
+                'image': len(img) > 0 and img.re(r'src="(.*?)"')[0] or None,
+            }
